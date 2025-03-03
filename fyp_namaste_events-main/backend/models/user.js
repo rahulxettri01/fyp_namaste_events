10c478b3d487e795cd4b35f37026d71acb7b12d3
rahulxettri01 <xettrirahul314@gmail.com>
2025-03-04 03:31:21 +0545

    mongodb connected to user and admin

--- fyp_namaste_events-main/backend/models/user.js
+++ fyp_namaste_events-main/backend/models/user.js
@@ -4,7 +4,7 @@ const joi = require("joi");
 const userSchema = new mongoose.Schema({
   userName: { type: String, required: true, unique: false },
   email: { type: String, required: true, unique: true },
-  phone: joi.string().regex(/^[0-9]{10}$/).messages({'string.pattern.base': `Phone number must have 10 digits.`}).required(),   // todo : phone not working
+  phone: joi.string().regex(/^[0-9]{10}$/).messages({'string.pattern.base': `Phone number must have 10 digits.`}).required(),   //  phone not working
   password: { type: String, required: true },
   role: { type: String, default: "user" },
   // role: { type: String, default: "user", enum: ["user", "admin"] },
